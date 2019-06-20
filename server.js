ea554106041b7a6819fdf3bab86b76fd772b3927
jepidoptera <jepidoptera@live.com>
2019-06-19 23:39:47 -0500

    added path module

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -11,7 +12,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 console.log("node env =", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
 // Add routes, both API and view
 app.use(routes);
